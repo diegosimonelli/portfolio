1033f0c68cae15b2021e6ba06012851068474e14
Diego Simonelli <dassimonelli@gmail.com>
2021-09-12 15:12:52 -0400

    resume updated

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -46,7 +46,7 @@ export default class AboutMe extends Component {
                 <p>
                   <a
                     className="button"
-                    href="DiegoSimonelliResume-2021.pdf"
+                    href="DiegoSimonelli-Resume.pdf"
                     download
                   >
                     <i className="fa fa-download" />
